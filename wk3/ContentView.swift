ebda666bd2ec029a3ccf5f2f27865bca4dde45ab
Ifedayo Adekoya <47520140+dayorolands@users.noreply.github.com>
2024-07-17 20:12:23 +0100

    Initial Commit

---
+++ wk3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wk3
+//
+//  Created by Dayo Adekoya on 7/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wk3/wk3App.swift
@@ -0,0 +1,17 @@
+//
+//  wk3App.swift
+//  wk3
+//
+//  Created by Dayo Adekoya on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct wk3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
