5665a191c1b9177e68a655edde155b78ad13424b
Ifedayo Adekoya <47520140+dayorolands@users.noreply.github.com>
2024-07-21 11:02:42 +0100

    A drawing app compatible withon all ios devices

--- wk3/ContentView.swift
+++ wk3/ContentView.swift
@@ -8,17 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentLine = Line()
+    @State private var lines: [Line] = []
+    @State private var selectedColor: Color = .blue
+    @State private var thickness: Double = 1.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Canvas { context, size in
+                for line in lines {
+                    var path = Path()
+                    path.addLines(line.points)
+                    context.stroke(path, with: .color(line.color), lineWidth: line.lineWidth)
+                }
+            }
+            .frame(minWidth: 400, minHeight: 400)
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+            .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                .onChanged({value in
+                    let newPoint = value.location
+                    currentLine.points.append(newPoint)
+                    self.lines.append(currentLine)
+                })
+                .onEnded({ value in
+                    self.lines.append(currentLine)
+                    self.currentLine = Line(points: [], color: selectedColor, lineWidth: thickness)
+                    })
+            )
+            
+            VStack {
+                HStack {
+                    Text("Thickness")
+                        .font(.headline)
+                        .padding(.trailing, 10)
+                    Slider(value: $thickness, in: 1...20)
+                        .frame(maxWidth: 200)
+                        .onChange(of: thickness) { [thickness] in
+                            currentLine.lineWidth = thickness
+                        }
+                    Divider()
+                    ColorPickerView(selectedColor: $selectedColor)
+                        .onChange(of: selectedColor) { [selectedColor] in
+                            currentLine.color = selectedColor
+                        }
+                }
+                Button(action: {
+                    lines.removeAll()
+                }) {
+                    Text("Clear Drawing")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding()
+            }
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previe: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ wk3/Preview Content/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  wk3
+//
+//  Created by Dayo Adekoya on 7/21/24.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct Icons {
+        static let plusCircle = "plus.circle"
+        static let line3HorizontalCircleFill = "line.3.horizontal.circle.fill"
+        static let circle = "circle"
+        static let circleInsetFilled = "circle.inset.filled"
+        static let exclamationMarkCircle = "exclamationmark.circle"
+        static let recordCircleFill = "record.circle.fill"
+        static let trayCircleFill = "tray.circle.fill"
+        static let circleFill = "circle.fill"
+    }
+}
